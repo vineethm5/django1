df77bd0218a8ce83be61e344f09c978b8b6b383f
vineethm5 <160957@sdmcujire.in>
2024-03-01 11:02:04 +0530

    added index.html page

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 def index(request):
-    return HttpResponse('Hello Vineeth')
+    htm=loader.get_template('index.html')
+    return HttpResponse(htm.render())
 
+def insert(request):
+    name=request.GET['name']
+    print(name)
+    htm=loader.get_template('index.html')
+    return HttpResponse(htm.render({"name":name}))
